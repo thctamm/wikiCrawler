cbbb6ebf94f59baadd29b7421df5597e2fe41131
timothy.tamm <tamm@college.harvard.edu>
2016-09-18 03:46:45 -0400

    add missing files

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from flask.ext.cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 import master
 import articlesearch as AS
 app = Flask(__name__)
