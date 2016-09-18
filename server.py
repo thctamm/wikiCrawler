49f1e27a94cbb2f440b0e50e953d083b79763280
timothy.tamm <tamm@college.harvard.edu>
2016-09-18 00:08:02 -0400

    allow cors

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify
+from flask.ext.cors import CORS, cross_origin
 import master
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/<source>/<target>")
+@cross_origin()
 def find(source, target):
         res = master.main(source, target)
         print(res)
