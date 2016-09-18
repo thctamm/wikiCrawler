f157515becc5817503c66a29c8d136826a481bd2
timothy.tamm <tamm@college.harvard.edu>
2016-09-18 03:33:41 -0400

    add in article search and refactor

--- server.py
+++ server.py
@@ -1,16 +1,36 @@
 from flask import Flask, jsonify
 from flask.ext.cors import CORS, cross_origin
 import master
+import article_search as AS
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/<source>/<target>")
+
+@app.route("wiki/<source>/<target>")
 @cross_origin()
-def find(source, target):
-        res = master.main(source, target)
-        print(res)
-        return jsonify(**res)
+def wiki(source, target):
+    res = master.main(source, target)
+    print("wiki: " + res)
+    return jsonify(**res)
+
+@app.route("views/")
+@cross_origin()
+def views():
+    res = {}
+    res["hillary"] = hillaryViews()
+    res["trump"] = trumpViews()
+    print("views: " + res)
+    return jsonify(**res)
+
+@app.route("articles/<source>")
+@cross_origin()
+def articles(source):
+    res = {}
+    res["hillary"] = hillaryArticles(source)
+    res["trump"] = trumpArticles(source)
+    print("articles: " + res)
+    return jsonify(**res)
 
 if __name__ == "__main__":
-        app.run()
+    app.run()
