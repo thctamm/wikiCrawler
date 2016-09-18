4c5b2f0fae4489ad97e5b864d70359589f1e0333
timothy.tamm <tamm@college.harvard.edu>
2016-09-17 22:57:57 -0400

    fix server

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/<source>/<target>")
 def find(source, target):
-        return master.main(source, target)
+        res = master.main(source, target)
+        print(res)
+        return res
 
 if __name__ == "__main__":
         app.run()
